8f83846a40b40faeea9819b3702d1f0e8a6a18d8
hys0254 <hys0254@naver.com>
2022-02-25 10:55:10 +0900

    programmers_python

--- RE/프로그래머스/가장 큰 정사각형.py
+++ RE/프로그래머스/가장 큰 정사각형.py
@@ -1,15 +1,13 @@
 
-# def solution(board):
-#     answer=0
-#     for row in board:
-#         answer=max(answer,max(row))
+def solution(board):
+    answer=0
+    for row in board:
+        answer=max(answer,max(row))
 
-#     for i in range(1,len(board)):
-#         for j in range(1,len(board[0])):
-#             if board[i][j]==1:
-#                 board[i][j]=min(min(board[i][j-1],board[i-1][j]),board[i-1][j-1])+1
-#                 answer=max(board[i][j],answer)
+    for i in range(1,len(board)):
+        for j in range(1,len(board[0])):
+            if board[i][j]==1:
+                board[i][j]=min(min(board[i][j-1],board[i-1][j]),board[i-1][j-1])+1
+                answer=max(board[i][j],answer)
 
-#     return answer**2
-a=[1,2,3,4,5,6,7]
-print(a.find(1))
+    return answer**2
