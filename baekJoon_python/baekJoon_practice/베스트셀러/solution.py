416d4a0fea7110fd2e5e4eee21a516e347f62450
hys0254 <hys0254@naver.com>
2022-04-09 14:25:10 +0900

    baekJoon_python

---
+++ baekJoon_python/baekJoon_practice/베스트셀러/solution.py
@@ -0,0 +1,13 @@
+caseCnt = int(input())
+books={}
+for _ in range(caseCnt):
+    book = input()
+    books[book]=books.get(book,0)+1
+max = max(books.values())
+ans =[]
+for book,cnt in books.items():
+    if cnt == max :
+        ans.append(book)
+ans.sort()
+for str in ans:
+    print(str)
