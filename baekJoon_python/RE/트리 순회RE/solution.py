badd0af0e227be872774f62ab5b65d52e0e9b646
hys0254 <hys0254@naver.com>
2022-04-21 20:48:10 +0900

    baekJoon_python

--- baekJoon_python/RE/트리 순회RE/solution.py
+++ baekJoon_python/RE/트리 순회RE/solution.py
@@ -1,42 +1,39 @@
-class Node:
-    def __init__(self,left_node,right_node,data):
+class Node :
+    def __init__(self,data,left_node,right_node):
         self.data=data
         self.left_node=left_node
         self.right_node=right_node
         
+        
 def pre_order(node):
-    print(node.data,end='')
-    if node.left_node !='.' :
-        pre_order(tree[node.left_node])
-    if node.right_node !='.' :
-        pre_order(tree[node.right_node])
+    print(tree[node].data,end='')
+    if tree[node].left_node != '.':
+        pre_order(tree[node].left_node)
+    if tree[node].right_node != '.':
+        pre_order(tree[node].right_node)
         
 def in_order(node):
-    
-    if node.left_node !='.' :
-        in_order(tree[node.left_node])
-    print(node.data,end='')
-    if node.right_node !='.' :
-        in_order(tree[node.right_node])
+    if tree[node].left_node != '.':
+        in_order(tree[node].left_node)
+    print(tree[node].data,end='')
+    if tree[node].right_node != '.':
+        in_order(tree[node].right_node)
         
 def post_order(node):
-    
-    if node.left_node !='.' :
-        post_order(tree[node.left_node])
-    if node.right_node !='.' :
-        post_order(tree[node.right_node])
-        
-    print(node.data,end='')
-    
-node_cnt= int(input())
+    if tree[node].left_node != '.':
+        post_order(tree[node].left_node)
+    if tree[node].right_node != '.':
+        post_order(tree[node].right_node)
+    print(tree[node].data,end='')
+
+node_cnt = int(input())
 tree={}
 for _ in range(node_cnt):
     root, left, right = input().split()
-    tree[root]=Node(left,right,root)
+    tree[root]=Node(root,left,right)
     
-pre_order(tree['A'])
+pre_order('A')
 print()
-in_order(tree['A'])
+in_order('A')
 print()
-post_order(tree['A'])
-    
+post_order('A')
--- baekJoon_python/RE/트리의 높이와 너비RE/solution.py
+++ baekJoon_python/RE/트리의 높이와 너비RE/solution.py
@@ -1,8 +1,53 @@
-node_cnt = int(input())
-tree=[]
-root, left, right = map(int, input().split())
-tree.append({root,1})
-print(tree.index(root))
-# for _ in range(node_cnt):
-#     root, left, right = map(int, input().split())
-#     tree.append
+class Node:
+    def __init__(self,data,left,right):
+        self.parent=-1
+        self.data=data
+        self.left_node=left
+        self.right_node=right
+        
+def in_order(node,level):
+    global level_depth,x
+    level_depth=max(level_depth,level)
+    if tree[node].left_node != -1:
+        in_order(tree[node].left_node,level+1)
+    min_level[level]=min(min_level.get(level,node_cnt),x)
+    max_level[level]=max(max_level.get(level,0),x)
+    x+=1
+    if tree[node].right_node != -1:
+        in_order(tree[node].right_node,level+1)
+
+x=1
+min_level={}
+max_level={}
+node_cnt=int(input())
+level_depth=1
+root=0
+tree={}
+
+for i in range(1,node_cnt+1):
+    tree[i]= Node(i,-1,-1)
+    
+for _ in range(node_cnt):
+    root,left,right=map(int,input().split())
+    tree[root].left_node=left
+    tree[root].right_node=right
+    if left != -1:
+        tree[left].parent=root
+    if right != -1:
+        tree[right].parent=root
+
+for i in range(1,node_cnt+1):
+    if tree[i].parent== -1:
+        root=i
+        break
+    
+in_order(root,1)
+result_level=1
+result_width=max_level[1]-min_level[1]
+
+for i in range(2,level_depth+1):
+    if result_width<max_level[i]-min_level[i]+1:
+        result_level=i
+        result_width=max_level[i]-min_level[i]+1
+        
+print(result_level,result_width)
