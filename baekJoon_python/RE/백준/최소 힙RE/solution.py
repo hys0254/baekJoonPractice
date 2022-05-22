1f613a993cfd99c7a851e988f27fd2ac900a2f38
hys0254 <hys0254@naver.com>
2022-05-22 10:55:10 +0900

    baekJoon_python

--- baekJoon_python/RE/백준/최소 힙RE/solution.py
+++ baekJoon_python/RE/백준/최소 힙RE/solution.py
@@ -1,3 +1,17 @@
+import sys
 import heapq
-n= int(input())
+n= int(sys.stdin.readline())
 heap=[]
+result=[]
+for _ in range(n):
+    input_num = int(sys.stdin.readline())
+    if input_num ==0:
+        if heap:
+            result.append(heapq.heappop(heap))
+        else:
+            result.append(0)
+    else:
+        heapq.heappush(heap,input_num)
+        
+for num in result:
+    print(num)
